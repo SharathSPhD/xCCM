a2e456cea7bcde624563aa1b51d17d8e78648037
ShaathSathish <qbz506@york.ac.uk>
2024-10-28 10:43:18 +0000

    Changes to be committed: modified:   __init__.py new file:   examples/__init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,22 @@
 # Save as: xCCM/__init__.py
 
 from .modules.core import EnhancedCCM, CCMResult
+from .modules.multivariate import MultivariateCCM
+from .modules.network import CCMNetwork
+from .modules.nonlinear import NonlinearityTests
+from .utils.preprocessing import DataPreprocessor
+from .utils.visualization import CCMVisualizer
+from .utils.performance import CCMPerformance
 
 __version__ = '0.2.0'
 
-__all__ = ['EnhancedCCM', 'CCMResult']
+__all__ = [
+    'EnhancedCCM',
+    'CCMResult',
+    'MultivariateCCM',
+    'CCMNetwork',
+    'NonlinearityTests',
+    'DataPreprocessor',
+    'CCMVisualizer',
+    'CCMPerformance'
+]
